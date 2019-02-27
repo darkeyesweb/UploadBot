d524de19d278a34f049cff1b2436c78e944c540f
Ghost <47865756+GhostlyCoding@users.noreply.github.com>
2019-02-27 13:42:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,49 +9,7 @@ var commands = require("./commands.js");
 const {Client,RichEmbed} = require('discord.js');
 const botjs = new Client();
 
-botjs.on('ready', () => {
-    console.log("Ghost Bot: I'm online and ready!");
-    const g = botjs.guilds.get("526111037573955584");
-
-    http.createServer((req, res) => {
-        console.log("HTTP request received");
-        var q = url.parse(req.url, true).query;
-        console.log(q);
-        botjs.channels.get(q.ch).send({
-            embed: {
-                author: {
-                    name: "Ghost Bot",
-                    icon_url: g.iconURL
-                },
-                title: q.title,
-                url: q.link,
-                description: q.desc,
-                fields: [{
-                        name: "Filesize: ",
-                        value: q.fs
-                    },
-                    {
-                        name: "Filename: ",
-                        value: q.fn
-                    },
-                    {
-                        name: "Uploader: ",
-                        value: q.un
-                    }
-                ],
-                timestamp: new Date(),
-                footer: {
-                    icon_url: botjs.user.avatarURL,
-                    text: "Ghost Bot | Made by <@395612767136251904> "
-                }
-            }
-        });
-    }).listen(8788);
-    console.log("HTTP Server Listening On Port 8788");
-});
-
-
-
+botjs.on('ready', () => {commands.readyHandle(botjs);});
 botjs.on('message', (m) => {commands.messageHandler(botjs,m);});
 botjs.on('guildMemberUpdate', (olduser,newuser) => {commands.checkRoleUpdates(olduser, newuser);});
 
