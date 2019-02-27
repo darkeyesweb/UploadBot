8c377f19326ad8826c3e32a1f78c842f0893e8eb
Ghost <47865756+GhostlyCoding@users.noreply.github.com>
2019-02-27 13:01:24 -0500

    Moving Event Handlers to commands.js

--- bot.js
+++ bot.js
@@ -6,10 +6,7 @@ var strip = require("stripchar").StripChar;
 var request = require('request');
 var commands = require("./commands.js");
 
-const {
-    Client,
-    RichEmbed
-} = require('discord.js');
+const {Client,RichEmbed} = require('discord.js');
 const botjs = new Client();
 
 botjs.on('ready', () => {
@@ -20,7 +17,6 @@ botjs.on('ready', () => {
         console.log("HTTP request received");
         var q = url.parse(req.url, true).query;
         console.log(q);
-        //botjs.channels.get(q.ch).send(q.un+"\n"+q.title+"\n"+q.desc+"\n"+q.link);
         botjs.channels.get(q.ch).send({
             embed: {
                 author: {
@@ -68,23 +64,6 @@ function isAlphaNumeric(str) {
 };
 
 
-function signup(u, p, uid) {
-
-    var g = botjs.guilds.get("526111037573955584");
-    console.log("Stripping username");
-    var un = strip.RSExceptUnsAlpNum(u);
-    console.log(un);
-    fs.appendFile("I:/xampp/AuthFiles/DiscordApp/users", "\n" + un + "::" + p + "::" + uid, "utf8", (err) => {
-        if (err) {
-            return "ERROR: " + err;
-        }
-
-        DMC.send(`Congratulations on becoming a Pisscord Member!\nYou now have the ability to upload files! To do so head over to https://pisscord.org/ and signin using these credentials:\n\`\`\`Username: ${un}\nPassword: ${pass}\`\`\`\n\nHappy Posting ;)`);
-        console.log('Ghost Bot: Created a new user! [' + un + ']');
-    });
-    return true;
-}
-
 botjs.on('message', (message) => {
     if (message.author.bot)
         return;
@@ -119,52 +98,5 @@ botjs.on('message', (message) => {
     }
 });
 
-botjs.on('guildMemberUpdate', function(old, newuser) {
-    console.log("Member Update Event");
-    var oldroles = [];
-    var newroles = [];
-
-    old.roles.forEach((role, key) => {
-        oldroles[role.id] = true;
-    });
-    newuser.roles.forEach((role, key) => {
-        newroles[role.id] = true;
-    });
-
-    if (oldroles["526113421943504927"] === true) {
-        return;
-    } else {
-        if (newroles["526113421943504927"] === true) {
-            fs.readFile("I:/xampp/AuthFiles/DiscordApp/users", "utf8", (err, data) => {
-                if (err)
-                    return console.log(err);
-                else
-                    console.log("User file read");
-
-                data.split("\n").forEach((val, i) => {
-                    if (val.split("::")[0] == newuser.user.username) {
-                        console.log("Ghost Bot: User already has active account");
-                        return false;
-                    }
-                });
-                newuser.createDM().then((DMC) => {
-                    fs.readFile("wordlist", 'utf8', (err, data) => {
-                        var min = 0;
-                        var max = data.split(",").length;
-                        var i1 = Math.floor(Math.random() * (+max - +min)) + min;
-                        var i2 = Math.floor(Math.random() * (+max - +min)) + min;
-
-                        var words = data.split(",");
-                        var word1 = words[i1].trim();
-                        var word2 = words[i2].trim();
-
-                        console.log("Calling sign-up function");
-                        signup(newuser.user.username, word1 + word2, newuser.user.id, true);
-                    });
-                });
-            });
-        }
-    }
-});
-
+botjs.on('guildMemberUpdate', (olduser,newuser) => {commands.checkRoleUpdates(olduser, newuser);});
 botjs.login(auth);
