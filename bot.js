b814b07147f29a3c058cdcbd8bc0c0cbba0d12f6
Ghost <47865756+GhostlyCoding@users.noreply.github.com>
2019-02-27 13:32:07 -0500

    Moving Event Handlers

--- bot.js
+++ bot.js
@@ -50,53 +50,9 @@ botjs.on('ready', () => {
     console.log("HTTP Server Listening On Port 8788");
 });
 
-function isAlphaNumeric(str) {
-    var code, i, len;
-    for (i = 0, len = str.length; i < len; i++) {
-        code = str.charCodeAt(i);
-        if (!(code > 47 && code < 58) && // numeric (0-9)
-            !(code > 64 && code < 91) && // upper alpha (A-Z)
-            !(code > 96 && code < 123)) { // lower alpha (a-z)
-            return false;
-        }
-    }
-    return true;
-};
 
 
-botjs.on('message', (message) => {
-    if (message.author.bot)
-        return;
-
-    console.log("Message Event");
-    var files = (message.attachments).array();
-    var g = botjs.guilds.get("526111037573955584");
-
-    if (message.channel.type == "dm") {
-        var args = message.content.split(" ");
-        if (args[0] == "/user") {
-            message.channel.startTyping();
-            commands.getuser(g, message);
-        } else if (args[0]) {
-            message.channel.startTyping();
-            commands.getpass(message);
-        }
-
-    } else {
-        if (files.length) {
-            files.forEach((a) => {
-                console.log("Attachment: " + a.url);
-                request(a.url).pipe(fs.createWriteStream("M:/Pisscord/attachments/" + a.filename));
-                var url = 'pisscord.org/attachments.php?fn=' + a.filename + '&uid=' + a.client + '&desc=' + a.message.content + '&ch=' + a.message.channel.id;
-                request(url, (err, req, body) => {
-                    console.log(err);
-                    console.log(req);
-                    console.log(body);
-                });
-            });
-        }
-    }
-});
-
+botjs.on('message', (m) => {commands.messageHandler(botjs,m);});
 botjs.on('guildMemberUpdate', (olduser,newuser) => {commands.checkRoleUpdates(olduser, newuser);});
+
 botjs.login(auth);
