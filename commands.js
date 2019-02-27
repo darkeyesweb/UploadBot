e25a0fda4e92294392033bd1233bbcd02ab5acb6
Ghost <47865756+GhostlyCoding@users.noreply.github.com>
2019-02-27 13:42:13 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -36,8 +36,49 @@ function isAlphaNumeric(str) {
 };
 
 ///////////////////////////
-//	  E X P O R T S			 //
+//     E X P O R T S     //
 ///////////////////////////
+
+exports.readyHandle(botjs) {
+    console.log("Ghost Bot: I'm online and ready!");
+    const g = botjs.guilds.get("526111037573955584");
+
+    http.createServer((req, res) => {
+        console.log("HTTP request received");
+        var q = url.parse(req.url, true).query;
+        console.log(q);
+        botjs.channels.get(q.ch).send({
+            embed: {
+                author: {
+                    name: "Ghost Bot",
+                    icon_url: g.iconURL
+                },
+                title: q.title,
+                url: q.link,
+                description: q.desc,
+                fields: [{
+                        name: "Filesize: ",
+                        value: q.fs
+                    },
+                    {
+                        name: "Filename: ",
+                        value: q.fn
+                    },
+                    {
+                        name: "Uploader: ",
+                        value: q.un
+                    }
+                ],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: botjs.user.avatarURL,
+                    text: "Ghost Bot | Made by <@395612767136251904> "
+                }
+            }
+        });
+    }).listen(8788);
+    console.log("HTTP Server Listening On Port 8788");
+}
 
 exports.getuser = function(g,m) {
 	
