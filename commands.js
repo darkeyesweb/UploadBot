2a0b75ec7588e5bce4b59046e218a3e7c8fbe881
Ghost <47865756+GhostlyCoding@users.noreply.github.com>
2019-02-27 13:31:55 -0500

    Moving Event Handlers

--- commands.js
+++ commands.js
@@ -1,6 +1,43 @@
 var fs = require('fs');
 var request = require('request');
+var mysql = require('mysql');
 var {Client,RichEmbed} = require('discord.js');
+
+
+function signup(u,p,uid) {
+    console.log("Stripping username");
+    var un = strip.RSExceptUnsAlpNum(u);
+    console.log(un);
+    fs.appendFile("I:/xampp/AuthFiles/DiscordApp/users", "\n" + un + "::" + p + "::" + uid, "utf8", (err) => {
+        if (err)
+            return "ERROR: " + err;
+				console.log('Ghost Bot: Created a new user! [' + un + ']');
+        return `Congratulations on becoming a Pisscord Member!\nYou now have the ability to upload files! To do so head over to https://pisscord.org/ and signin using these credentials:\n\`\`\`Username: ${un}\nPassword: ${pass}\`\`\`\n\nHappy Posting ;)`;
+    });
+    return true;
+}
+
+function setPassword(g,i,m) {
+	
+}
+
+
+function isAlphaNumeric(str) {
+    var code, i, len;
+    for (i = 0, len = str.length; i < len; i++) {
+        code = str.charCodeAt(i);
+        if (!(code > 47 && code < 58) && // numeric (0-9)
+            !(code > 64 && code < 91) && // upper alpha (A-Z)
+            !(code > 96 && code < 123)) { // lower alpha (a-z)
+            return false;
+        }
+    }
+    return true;
+};
+
+///////////////////////////
+//	  E X P O R T S			 //
+///////////////////////////
 
 exports.getuser = function(g,m) {
 	
@@ -83,4 +120,87 @@ exports.attachments = function(g,m) {
 			}
 		});
 	}).catch(console.error);
+}
+
+exports.checkRoleUpdates = function(old, newuser) {
+    console.log("Member Update Event");
+    var oldroles = [];
+    var newroles = [];
+
+    old.roles.forEach((role, key) => {
+        oldroles[role.id] = true;
+    });
+    newuser.roles.forEach((role, key) => {
+        newroles[role.id] = true;
+    });
+
+    if (oldroles["526113421943504927"] === true) {
+        return;
+    } else {
+        if (newroles["526113421943504927"] === true) {
+            fs.readFile("I:/xampp/AuthFiles/DiscordApp/users", "utf8", (err, data) => {
+                if (err)
+                    return console.log(err);
+                else
+                    console.log("User file read");
+
+                data.split("\n").forEach((val, i) => {
+                    if (val.split("::")[0] == newuser.user.username) {
+                        console.log("Ghost Bot: User already has active account");
+                        return false;
+                    }
+                });
+                newuser.createDM().then((DMC) => {
+                    fs.readFile("wordlist", 'utf8', (err, data) => {
+                        var min = 0;
+                        var max = data.split(",").length;
+                        var i1 = Math.floor(Math.random() * (+max - +min)) + min;
+                        var i2 = Math.floor(Math.random() * (+max - +min)) + min;
+
+                        var words = data.split(",");
+                        var word1 = words[i1].trim();
+                        var word2 = words[i2].trim();
+
+                        console.log("Calling sign-up function");
+                        DMC.send(signup(newuser.user.username, word1 + word2, newuser.user.id, true));
+                    });
+                });
+            });
+        }
+    }
+}
+
+exports.messageHandler = function(b,m) {
+	if (m.author.bot)
+        return;
+
+    console.log("Message Event");
+    var files = (m.attachments).array();
+    var g = b.guilds.get("526111037573955584");
+
+    if (m.channel.type == "dm") {
+        var args = m.content.split(" ");
+        if (args[0] == "/user") {
+            m.channel.startTyping();
+            exports.getuser(g, m);
+        } else if (args[0]) {
+            m.channel.startTyping();
+            exports.getpass(m);
+        }
+
+    } else {
+        if (files.length) {
+            files.forEach((a) => {
+                console.log("Attachment: " + a.url);
+                request(a.url).pipe(fs.createWriteStream("M:/Pisscord/attachments/" + a.filename));
+                var url = 'pisscord.org/attachments.php?fn=' + a.filename + '&uid=' + a.client + '&desc=' + a.message.content + '&ch=' + a.message.channel.id;
+                request(url, (err, req, body) => {
+                    console.log(err);
+                    console.log(req);
+                    console.log(body);
+                });
+            });
+        }
+    }
+	}	
 }
